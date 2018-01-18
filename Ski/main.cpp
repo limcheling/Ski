857d065d3396ca41f026ec99fc72a44d0fc31782
Lim Che Ling <limcheling@yahoo.com>
2018-01-18 10:38:43 +0800

    Initial Commit

---
+++ Ski/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ski
+//
+//  Created by Cheling Lim on 18/1/18.
+//  Copyright © 2018 Cheling Lim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
